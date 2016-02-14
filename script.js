b0a729983e46dc5eabef8ac3a1bae311722cefc7
jrlandis <landis.jordan@gmail.com>
2016-02-14 18:19:04 -0500

    corrected nick link

--- script.js
+++ script.js
@@ -8,4 +8,5 @@ document.getElementById("nav01").innerHTML =
 "<li><a href='about.html'>About</a></li>" +
 "<li><a href='nickBio.html'>Nick Pesta</a></li>" +
 "<li><a href='bio.html'>Jordan</a></li>" +
+"<li><a href='nickBio.html'>Nick Pesta</a></li>" +
 "</ul>";
