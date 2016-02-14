a316721144b84b2a1e35c89634ce7bd72fd467a0
Nick Pesta <njpesta@gmail.com>
2016-02-14 14:44:10 -0500

    Changed JS
    Updated copyrighgt in JS. Added Nick tab to page navigation

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.getElementById("foot01").innerHTML = 
-"<p>&copy; " + new Date().getFullYear() + " Nick Pesta. All Rights Reserved.</p>";
+"<p>&copy; " + new Date().getFullYear() + " Jordan Landis & Nick Pesta. All Rights Reserved.</p>";
 
 document.getElementById("nav01").innerHTML = 
 "<ul id='menu'>" +
 "<li><a href= 'index.html'>Home</a></li>" +
 "<li><a href= 'customers.html'>Data</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
+"<li><a href='nickBio.html'>Nick Pesta/a></li>" +
 "</ul>";
